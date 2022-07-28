73d62225a05de028c706316e59440c8b8e69c801
Joseph Rodriguez <joseph.rodriguez@localpolroots-MacBook-Pro.local>
2022-07-28 15:10:22 -0500

    configuracion completa cloud

--- transactional-ms/src/main/java/com/payu/myshop/transactionalms/TransactionalMsApplication.java
+++ transactional-ms/src/main/java/com/payu/myshop/transactionalms/TransactionalMsApplication.java
@@ -2,9 +2,11 @@ package com.payu.myshop.transactionalms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableEurekaClient
 @PropertySource("classpath:variables.yml")
 public class TransactionalMsApplication {
 
--- ventas-ms/src/main/java/com/payu/myshop/ventasms/VentasMsApplication.java
+++ ventas-ms/src/main/java/com/payu/myshop/ventasms/VentasMsApplication.java
@@ -2,8 +2,11 @@ package com.payu.myshop.ventasms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VentasMsApplication {
 
 	public static void main(String[] args) {
